974bd58ba3486889429a9225756fc37b528b4053
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-05-08 19:01:19 +0900

    feat: creates user by statefull way

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   if (method === "GET" && url === "/users") {
-    return res.end("listagem de usuarios");
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
   if (method === "POST" && url === "/users") {
-    return res.end("CRIACAO de usuarios");
+    users.push({
+      id: 1,
+      name: "gabriel",
+      email: "g@g.com",
+    });
   }
 
   return res.end("Hello world");
