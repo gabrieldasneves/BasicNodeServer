58911aacd1a4fe5b600acf06147da3fca0ef370a
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-05-10 00:05:17 +0900

    feat: creates node streams writable and transform

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -1,4 +1,4 @@
-import { Readable } from "node:stream";
+import { Readable, Writable, Transform } from "node:stream";
 
 class oneToHundredStream extends Readable {
   index = 1;
@@ -15,6 +15,22 @@ class oneToHundredStream extends Readable {
   }
 }
 
-new oneToHundredStream().pipe(process.stdout);
+class multiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10);
+    callback();
+  }
+}
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1;
+    callback(null, Buffer.from(String(transformed)));
+  }
+}
+
+new oneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new multiplyByTenStream());
 
 //run : node streams/fundamentals.js
