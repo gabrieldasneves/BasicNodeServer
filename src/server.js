bbfb9a960028a9200b6393445295be81bedd5e91
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-05-13 22:48:07 +0900

    feat: creates middleware to intercept and transform req and res from routes

--- src/server.js
+++ src/server.js
@@ -1,24 +1,15 @@
 import http from "node:http";
+import { json } from "../middlewares/json.js";
 
 const users = [];
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
-  const buffers = [];
 
-  for await (const chunk of req) {
-    buffers.push(chunk);
-  }
+  await json(req, res);
 
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString());
-  } catch {
-    req.body = null;
-  }
   if (method === "GET" && url === "/users") {
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
+    return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
