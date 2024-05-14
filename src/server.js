6d36ec00c5b43c55ba531e8e01e4d7bd4ba077c0
gabrielalvesbrazil <125869083+gabrielalvesbrazil@users.noreply.github.com>
2024-05-14 23:34:49 +0900

    feat: implements persistent database

--- src/server.js
+++ src/server.js
@@ -1,24 +1,28 @@
 import http from "node:http";
 import { json } from "../middlewares/json.js";
+import { Database } from "../middlewares/database.js";
 
-const users = [];
-
+const database = new Database();
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
   if (method === "GET" && url === "/users") {
+    const users = database.select("users");
+
     return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
     const { name, email } = req.body;
-    users.push({
+    const user = {
       id: 1,
       name: name,
       email: email,
-    });
+    };
+
+    database.insert("users", user);
 
     return res.writeHead(201).end();
   }
